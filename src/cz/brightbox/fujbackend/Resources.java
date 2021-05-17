9cc6d75e4817c5524f62e3cd9f410ffc56718a7e
Chesters-CZ <62833819+Chesters-CZ@users.noreply.github.com>
2021-05-17 10:57:58 +0200

    mělo by to fungovat

---
+++ src/cz/brightbox/fujbackend/Resources.java
@@ -0,0 +1,18 @@
+package cz.brightbox.fujbackend;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.io.FileNotFoundException;
+
+@Path("resources")
+public class Resources {
+
+    @GET
+    public Response getPrices() {
+        try {
+            return Response.ok(JsonLoader.loadJson("data.json")).build();
+        } catch (Exception e) {
+            return Response.status(500, "Soubor nenalezen.").build();
+        }
+    }
+}
